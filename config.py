1ca370eb3ee55de4bccc24114e2146da4b07c308
Liam Eime <liameime@outlook.com>
2023-12-05 16:19:03 +1000

    Transition to using environment variables
    To keep the main.py script centered on the HTTP methods the CR1000 uses to send data, the program has been reorganized to use environment variables to pass in directories and filenames, etc.

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    output_dir: str
+    temp_low_freq_filename: str
+    temp_high_freq_filename: str
+    output_low_freq_filename: str
+    output_high_freq_filename: str
+    max_low_freq_data_rows: int
+    model_config = SettingsConfigDict(env_file=".env")
